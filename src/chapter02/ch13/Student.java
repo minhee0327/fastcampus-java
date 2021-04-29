1b468d6a11a5aa405aebaa84102fa9cccccd6b59
minhee <queen.minhee@gmail.com>
2021-04-29 22:38:29 +0900

    chapter02.ch13 객체 간의 협력 (collaboration)

---
+++ src/chapter02/ch13/Student.java
@@ -0,0 +1,25 @@
+package chapter02.ch13;
+
+public class Student {
+    String studentName;
+    int money;
+
+    Student(String studentName, int money){
+        this.studentName = studentName;
+        this.money = money;
+    }
+
+    public void takeBus(Bus bus){
+        bus.take(1000);
+        this.money -=1000;
+    }
+
+    public void takeSubway(Subway subway){
+        subway.take(1200);
+        this.money -=1200;
+    }
+
+    public void showInfo(){
+        System.out.println(studentName + " 님의 남은 돈은 " + money +"원 입니다.");
+    }
+}
