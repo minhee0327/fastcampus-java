e95290b17d7d13daaac901a3e6dac56fd943b051
minhee <queen.minhee@gmail.com>
2021-05-04 14:58:41 +0900

    chapter03.ch11 interface

---
+++ src/chapter03/ch11/CalculatorTest.java
@@ -0,0 +1,16 @@
+package chapter03.ch11;
+
+public class CalculatorTest {
+    public static void main(String[] args) {
+        CompleteCalc calc = new CompleteCalc();
+        int num1 = 10;
+        int num2 = 2;
+
+        System.out.println(calc.add(num1, num2));
+        System.out.println(calc.substract(num1, num2));
+        System.out.println(calc.times(num1, num2));
+        System.out.println(calc.divide(num1, num2));
+
+        calc.showInfo();
+    }
+}
